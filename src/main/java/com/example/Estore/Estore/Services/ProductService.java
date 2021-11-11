baecf236ef78f530695cbb05e05812582e6c430d
Anila Sebastian <anilaseban5001@gmail.com>
2021-11-11 10:06:55 +0530

    productcommit

---
+++ src/main/java/com/example/Estore/Estore/Services/ProductService.java
@@ -0,0 +1,13 @@
+package com.example.Estore.Estore.Services;
+
+import com.example.Estore.Estore.Shared.dto.Product.ProductDto;
+import com.example.Estore.Estore.Shared.dto.User.UserDto;
+
+public interface ProductService {
+    ProductDto createProduct(ProductDto product);
+
+
+    }
+
+
+
