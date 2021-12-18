f4a57141a0030ff32cb592b98226864bab2fe391
AMAL GEORGE <amalgeorge95@gmail.com>
2021-12-18 20:41:48 +0530

    updated13

--- src/main/java/com/example/Estore/Estore/io/Repositories/WishList/WishListRepository.java
+++ src/main/java/com/example/Estore/Estore/io/Repositories/WishList/WishListRepository.java
@@ -13,9 +13,7 @@ import java.util.Optional;
 @Repository
 public interface WishListRepository extends CrudRepository<WishListEntity, Long> {
 
-    Optional<WishListEntity> findAllByUserEntity(UserEntity userEntity);
-
-    WishListEntity findByUserEntity(UserEntity userEntity);
+    WishListEntity findAllByWishListId(Long wishListId);
 
     /**
      * Method to delete product from the wishlist.
@@ -24,10 +22,23 @@ public interface WishListRepository extends CrudRepository<WishListEntity, Long>
      */
     @Transactional
     @Modifying
-
     @Query(value = "DELETE FROM wishlist_products w WHERE w.wish_list_id=?1 AND w.product_id=?2",nativeQuery = true)
     void deleteProduct(Long wishListId, Long productId);
 
-    @Query(value = "SELECT * FROM wishlist w WHERE w.user_reference_id=?1", nativeQuery = true)
+    /**
+     * method to find wishlist details based on userEntity.
+     * @param userEntity
+     * @return
+     */
+    WishListEntity findByUserEntity(UserEntity userEntity);
+
+    Optional<WishListEntity> findAllByUserEntity(UserEntity userEntity);
+
+    /**
+     * method to find wishlist baseed on user's id.
+     * @param id
+     * @return
+     */
+    @Query(value ="SELECT * FROM wishlist w WHERE w.user_reference_id=?1",nativeQuery = true)
     WishListEntity findByUserId(long id);
 }
